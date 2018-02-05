77b9618bdba67546b7cd3f005442ddd9537fff2d
Fahad Suhail Ahmad <fahadahmad2001@outlook.com>
2018-02-05 20:15:05 +0300

    Add project files.

---
+++ ISO Mounter/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ISO_Mounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Optimus", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Free Image Mounter";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(605, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Mount Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(72, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 199);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(422, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(135, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(414, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "List of mounted images (click to dismount)";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "All Supported Files (*.vhd, *.iso, *.vhdx)|*.iso; *.vhd; *.vhdx";
+            this.openFileDialog1.Title = "Please select the image file";
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 550F;
+            this.Column1.HeaderText = "Path to image file";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 550;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 360);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ ISO Mounter/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.IO;
+//using System.Runtime.
+
+namespace ISO_Mounter
+{
+    public partial class Form1 : Form
+    {
+        public string FileLocation;
+        public string command1;
+        public string command2;
+        public StreamWriter editBAT;
+        public string output1;
+        public string error1;
+        //public int unmountresponse;
+        //public Process MountImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Status: Selecting file";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                FileLocation = openFileDialog1.FileName;
+                label2.Text = FileLocation;
+                label3.Text = "Status: ";
+            }
+            else
+            {
+                label3.Text = "Status: ";
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            command1 = "Mount-DiskImage -ImagePath " + "'" + FileLocation + "'";
+            //using (PowerShell PowerShellInstance = PowerShell.Create())
+            //{
+            //  PowerShellInstance.AddCommand("Mount-DiskImage");
+            //PowerShellInstance.AddParameter();
+            //PowerShellInstance.AddArgument(" -ImagePath " + "'" + FileLocation + "'");
+            // PowerShellInstance.Invoke();
+
+            //}
+            editBAT = new StreamWriter(Application.StartupPath + "\\" + "MountISO.ps1");
+            editBAT.WriteLine(command1);
+            editBAT.Close();
+            command2 = "powershell.exe -command " + command1;
+            //MessageBox.Show(command2);
+            System.Diagnostics.Process MountImage = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo MountImageInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            MountImageInfo.CreateNoWindow = true;
+            MountImageInfo.UseShellExecute = false;
+            MountImageInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            MountImageInfo.Arguments = "/c " + command2;
+            //MountImageInfo.RedirectStandardError = true;
+            //MountImageInfo.RedirectStandardOutput = true;
+            MountImage.StartInfo = MountImageInfo;
+            label3.Text = "Status: Mounting";
+            MountImage.Start();
+            //output1 = MountImage.StandardOutput.ReadToEnd();
+            //error1 = MountImage.StandardError.ReadToEnd();
+            //MessageBox.Show(output1);
+            //MessageBox.Show(error1);
+            MountImage.WaitForExit();
+            MountImage.Close();
+            dataGridView1.Rows.Add(FileLocation);
+            label3.Text = "Status:";
+            MessageBox.Show("Successfully mounted");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DialogResult unmountresponse  = MessageBox.Show("Do you really want to unmount this image","Confirm dismount",MessageBoxButtons.YesNo);
+            if( unmountresponse == DialogResult.Yes){
+                int CellIndex = dataGridView1.CurrentCell.RowIndex;
+                String removeFile = dataGridView1.CurrentCell.Value.ToString();
+                String command3 = "Dismount-DiskImage -ImagePath " + "'" + removeFile + "'";
+                String command4 = "/c powershell.exe -command " + command3;
+                System.Diagnostics.Process UnMountImage = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo UnMountImageInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+                UnMountImageInfo.CreateNoWindow = true;
+                UnMountImageInfo.UseShellExecute = false;
+                UnMountImageInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                UnMountImageInfo.Arguments = command4;
+                UnMountImage.StartInfo = UnMountImageInfo;
+                label3.Text = "Status: Unmounting";
+                UnMountImage.Start();
+                UnMountImage.WaitForExit();
+                UnMountImage.Close();
+                dataGridView1.Rows.RemoveAt(CellIndex);
+                label3.Text = "Status:";
+                MessageBox.Show("Successfully unmounted");
+            }
+            else
+            {
+
+            }
+            //MessageBox.Show(dataGridView1.CurrentCell.Value.ToString());
+        }
+    }
+}
